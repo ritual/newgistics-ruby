425839dbb5afe7e7601b0c9ec433c6313560acb2
Daniel Archer <me@dja.io>
2018-03-28 22:13:37 -0700

    label info at the top level for orders

--- lib/newgistics/order.rb
+++ lib/newgistics/order.rb
@@ -5,6 +5,22 @@ module Newgistics
     attribute :id, String
     attribute :warehouse_id, String
     attribute :customer, Customer
+
+    attribute :company, String
+    attribute :name, String
+    attribute :first_name, String
+    attribute :last_name, String
+    attribute :email, String
+    attribute :address1, String
+    attribute :address2, String
+    attribute :city, String
+    attribute :state, String
+    attribute :country, String
+    attribute :phone, String
+    attribute :fax, String
+    attribute :zip, String
+    attribute :is_residential, Boolean
+
     attribute :drop_ship_info, Hash
     attribute :order_date, Date
     attribute :ship_method, String
